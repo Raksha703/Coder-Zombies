e015ca60c31a4e317165713ea10d7f22690faadb
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-01-17 00:09:55 +0530

    Update detect.py

--- backend/detect.py
+++ backend/detect.py
@@ -8,7 +8,7 @@ import numpy as np
 face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 classifier =load_model('./Emotion_Detection.h5')
 
-class_labels = ['Bored','Happy','Neutral','Sad','Confused']
+class_emotion = ['Bored', 'Happy', 'Neutral', 'Confused','Surprise']
 
 cap = cv2.VideoCapture(0)
 
@@ -17,11 +17,9 @@ while True:
     labels = []
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
-
     for (x,y,w,h) in faces:
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-
 
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
@@ -29,12 +27,10 @@ while True:
             roi = np.expand_dims(roi,axis=0)
 
             preds = classifier.predict(roi)[0]
-            print("\nprediction = ",preds)
-            label=class_labels[preds.argmax()]   #this label will contain emotion of maximum percentage
-            print("\nprediction max = ",preds.argmax())
-            print("\nlabel = ",label)
-            label_position = (x,y)
-            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+            emotion=class_emotion[preds.argmax()]   #this label will contain emotion of maximum percentage
+            print("\nlabel = ",emotion)
+            emotion_position = (x,y)
+            cv2.putText(frame,emotion,emotion_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
         else:
             cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
         print("\n\n")
